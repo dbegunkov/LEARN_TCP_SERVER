82fd0393decfffc8bc1ef838d1dab48828098c62
Dmitry Begunkov <dbegunkov@gmail.com>
2016-11-24 01:29:12 +0300

    nginx.conf

--- mkd.sh
+++ mkd.sh
@@ -2,6 +2,6 @@ mkdir /home/box/web
 mkdir /home/box/web/public
 mkdir /home/box/web/public/img
 mkdir /home/box/web/public/css
-mkdir /home/box/web/public/gc
+mkdir /home/box/web/public/jc
 mkdir /home/box/web/uploads
 mkdir /home/box/web/etc
